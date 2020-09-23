35581dd336cd0620776a3ac96b5b04c420ababfd
kurogi yuki <xtoriymoonz2@yahoo.co.jp>
2020-09-23 15:45:54 +0900

    add users_new,create(新規投稿機能)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
 end
