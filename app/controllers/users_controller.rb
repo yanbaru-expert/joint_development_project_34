e4b4fcd03f367e8af1ec81e7e540beb856dd7863
kurogi yuki <xtoriymoonz2@yahoo.co.jp>
2020-09-23 16:26:40 +0900

    add users_show(詳細表示),new(新規投稿画面を修正)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
     User.create(user_params)
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :age)
