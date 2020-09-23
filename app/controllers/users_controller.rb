6804edaebff5e978340b2880e99a3cc59d2e706e
kurogi yuki <xtoriymoonz2@yahoo.co.jp>
2020-09-23 16:52:11 +0900

    add destroy(削除機能)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,11 @@ class UsersController < ApplicationController
     user.update(user_params)
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :age)
