35bb7d06339684e44f48c7037b2b7144b5c6cd74
kurogi yuki <xtoriymoonz2@yahoo.co.jp>
2020-09-23 16:42:45 +0900

    add users_edit,update(編集機能)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :age)
