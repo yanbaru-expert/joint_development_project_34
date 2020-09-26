ebf497d3c7a00fde9d037958646b98d00899e9e3
Kenichi Shimada <tobeshimachan@yahoo.co.jp>
2020-09-26 18:39:06 +0900

    new and create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,19 @@
 class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+  
+  def new
+    @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:title, :content)
+  end
+
 end
