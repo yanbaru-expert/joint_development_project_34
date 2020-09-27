87ee522b9a04aa6da85055c878e45ba7284c0514
satokoyo <thebeatles.0605@gmail.com>
2020-09-27 18:39:54 +0900

    rails g model コマンドで「マイグレーションファイル」と「モデル」を作成し，マイグレーションファイルに必要なカラムを追加し，マイグレーションを行う

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,13 @@ class TasksController < ApplicationController
     def new
         @task = Task.new
     end
+
+    def create
+        Task.create(task_params)
+    end
+
+    private
+    def task_params
+      params.require(:task).permit(:title, :content)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_220029) do
+ActiveRecord::Schema.define(version: 2020_09_27_093106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
