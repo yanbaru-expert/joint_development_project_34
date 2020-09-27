c88209016439cb275d3ecba7c2f1bd994d20242c
satokoyo <thebeatles.0605@gmail.com>
2020-09-27 21:06:34 +0900

    詳細表示機能 show を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,6 +11,10 @@ class TasksController < ApplicationController
         Task.create(task_params)
     end
 
+    def show
+        @task = Task.find(params[:id])
+    end
+
     private
     def task_params
       params.require(:task).permit(:title, :content)
